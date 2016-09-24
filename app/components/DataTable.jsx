4b8dce7865b4310a290a524a0c4a0544860dfba4
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2016-09-23 17:15:39 -0700

    Start getMetrics

--- app/components/DataTable.jsx
+++ app/components/DataTable.jsx
@@ -3,36 +3,80 @@ var FixedDataTable = require('fixed-data-table');
 var adObject = require('adObject');
 const {Table, Column, Cell} = FixedDataTable;
 
-// const adObjectData = {
-//     "ads": [
-//         {
-//             "id": 1,
-//             "remote_id": "123",
-//             "name": "123",
-//             "status": "ACTIVE",
-//         },
-//         {
-//             "id": 2,
-//             "remote_id": "456",
-//             "name": "456",
-//             "status": "ACTIVE",
-//
-//         },
-//         {
-//             "id": 3,
-//             "remote_id": "789",
-//             "name": "789",
-//             "status": "ACTIVE",
-//         },
-//         {
-//             "id": 4,
-//             "remote_id": "901",
-//             "name": "901",
-//             "status": "ACTIVE",
-//         },
-//     ]
-// };
+const adMetrics = {
+    "column_names": [
+        "impressions",
+        "reach",
+        "frequency",
+        "cpm",
+        "spend",
+        "ctr",
+        "cost_per_inline_link_click",
+        "actions:goal",
+        "actions:link_click",
+        "cost_per_action_type:cost_per_goal",
+        "actions:offsite_conversion"
+    ],
+    "rows": [
+        {
+            "remote_id": "456",
+            "impressions": "123",
+            "reach": 123,
+            "frequency": 1.0413449389302,
+            "cpm": 12.30131445905,
+            "spend": 182.49,
+            "ctr": 0.87630603303,
+            "cost_per_inline_link_click": 3.0415,
+            "actions:goal": 3,
+            "actions:link_click": 60,
+            "cost_per_action_type:cost_per_goal": 60.83,
+            "actions:offsite_conversion": 456
+        },
+        {
+            "remote_id": "123",
+            "impressions": "123",
+            "reach": 123,
+            "frequency": 1.0413449389302,
+            "cpm": 12.30131445905,
+            "spend": 182.49,
+            "ctr": 0.87630603303,
+            "cost_per_inline_link_click": 3.0415,
+            "actions:goal": 3,
+            "actions:link_click": 60,
+            "cost_per_action_type:cost_per_goal": 60.83,
+            "actions:offsite_conversion": 123
+        },
+        {
+            "remote_id": "789",
+            "impressions": "123",
+            "reach": 123,
+            "frequency": 1.0413449389302,
+            "cpm": 12.30131445905,
+            "spend": 182.49,
+            "ctr": 0.87630603303,
+            "cost_per_inline_link_click": 3.0415,
+            "actions:goal": 3,
+            "actions:link_click": 60,
+            "cost_per_action_type:cost_per_goal": 60.83,
+            "actions:offsite_conversion": 789
+        },
+        {
+            "remote_id": "901",
+            "impressions": "123",
+            "reach": 123,
+            "frequency": 1.0413449389302,
+            "cpm": 12.30131445905,
+            "spend": 182.49,
+            "ctr": 0.87630603303,
+            "cost_per_inline_link_click": 3.0415,
+            "actions:goal": 3,
+            "actions:link_click": 60,
+            "cost_per_action_type:cost_per_goal": 60.83,
+            "actions:offsite_conversion": 901
+        }
+    ]
 
+}
 
 class DataTable extends React.Component {
 
@@ -61,37 +105,53 @@ class DataTable extends React.Component {
     // }
 
 
+    getRowData:
+
     render() {
         // console.log(adObjectData);
         var that = this;
         // var colName = this.getColNames(adObjectData)
-        var colName = adObject.getColNames()
+        var rowName = adObject.getColNames()
         console.log('colName: ' + colName)
+
 
         return (
             <div>
-                <button className="button">Fill Chart</button>
 
                 <Table
                     rowsCount={this.state.myTableData.length}
                     rowHeight={50}
                     headerHeight={50}
-                    width={500}
+                    width={700}
                     height={500}>
-                    {colName.map(function(name, i){
-                            return (
-                                <Column
-                                    header={<Cell>{name}</Cell>}
-                                    cell={props => (
-                                        <Cell {...props}>
-                                            {that.state.myTableData[props.rowIndex].name}
-                                        </Cell>
-                                    )}
-                                    width={200}
-                                />
-                            )
-                        }
-                    )}
+                    <Column
+                        header={<Cell>AC NAME</Cell>}
+                        cell={props => (
+                            colName.map(function(name, i) {
+                                console.log(name, i)
+                                return (
+                                    <Cell {...props}>
+                                        {name}
+                                    </Cell>
+                                )
+                            })
+                        )}
+                        width={200}
+                    />
+                    <Column
+                        header={<Cell>{adMetrics.column_names}</Cell>}
+                        cell={props => (
+                            colName.map(function(name, i) {
+                                console.log(name, i)
+                                return (
+                                    <Cell {...props}>
+                                        {name}
+                                    </Cell>
+                                )
+                            })
+                        )}
+                        width={200}
+                    />
                 </Table>
             </div>
         );
